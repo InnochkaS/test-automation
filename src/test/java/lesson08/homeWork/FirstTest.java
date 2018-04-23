a8e86079fe509105f8b7f7fc34be88444ff41ca8
Inna <sotnyk.inna@gmail.com>
2018-04-23 08:47:54 +0300

    Homework for lesson #8 is completed.

--- src/test/java/lesson08/homeWork/FirstTest.java
+++ src/test/java/lesson08/homeWork/FirstTest.java
@@ -1,46 +1,64 @@
 package lesson08.homeWork;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.Logs;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 
 @RunWith(JUnit4.class)
 public class FirstTest extends BaseTest {
 
-    private static final Logger LOG = LogManager.getLogger(FirstTest.class);
+    //private static final Logger LOG = LogManager.getLogger(FirstTest.class);
 
     @Test
-    public void test_warning(){
-        open("http://automationpractice.com/index.php?controller=authentication&back=my-account");
-        assertThat(ExpectedConditions.titleContains("Login"));
-        Logs logs = getWebDriver().manage().logs();
-        LogEntries logEntries = logs.get(LogType.BROWSER);
+    public void test_buying() {
 
-        for (LogEntry logEntry : logEntries) {
-            String logMessage = logEntry.getMessage();
-            if (logMessage.contains("credit card")) {
-                LOG.info(logMessage);
-            }
-        }
+        String query = "Printed Summer Dress";
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.visit();
+        assertThat(ExpectedConditions.titleContains("My Store"));
+        mainPage.enterQuery(query);
+        assertThat(textToBePresentInElementLocated(mainPage.ADVICE_LOCATOR, query));
+        driver.findElement(By.name("submit_search")).click();
+        assertThat(ExpectedConditions.titleContains("Search - My Store"));
+        driver.findElement(By.linkText("Printed Summer Dress")).click();
+        assertThat(ExpectedConditions.titleContains("Printed Summer Dress - My Store"));
+        driver.findElement(By.id("add_to_cart")).click();
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("layer_cart")));
+        driver.findElement(By.linkText("Proceed to checkout")).click();
+        driver.findElement(By.linkText("Proceed to checkout")).click();
+        mainPage.login();
+        driver.findElement(By.name("processAddress")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("uniform-cgv"))).click();
+        driver.findElement(By.name("processCarrier")).click();
+        assertEquals("PLEASE CHOOSE YOUR PAYMENT METHOD", driver.findElement(By.xpath("//*[@id=\"center_column\"]/h1")).getText());
+        driver.findElement(By.xpath("//*[@id=\"HOOK_PAYMENT\"]/div[1]/div/p/a")).click();
+        assertEquals("ORDER SUMMARY", driver.findElement(By.xpath("//*[@id=\"center_column\"]/h1")).getText());
+        driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button")).click();
+        assertEquals("Your order on My Store is complete.", driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/p/strong\n")).getText());
+
+        WebElement we = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div"));
+        String str = we.getText();
+        String orderRef = str.substring(str.indexOf("your order reference ") + "your order reference ".length(), str.indexOf(" in the subject of your bank wire"));
+
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).click();
+        assertEquals("MY ACCOUNT", driver.findElement(By.xpath("//*[@id=\"center_column\"]/h1")).getText());
+        driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div[1]/ul/li[1]/a/span")).click();
+
+        assertEquals(orderRef, driver.findElement(By.xpath("//*[@id=\"order-list\"]/tbody/tr[1]/td[1]")).getText());
     }
-
-
 }
 
-// 1. Создать тестовый класс со следующим тестом: поиск товара,
-// добавление его в корзину, оформление заказа, покупка, проверка, что в истории заказов появился ИМЕННО ваш заказ.
+// 1. Создать тестовый класс со следующим тестом:
+// поиск товара, добавление его в корзину, оформление заказа, покупка, проверка, что в истории заказов появился ИМЕННО ваш заказ.
 // 2. Сделать логирование к методам пейдж-объектов.
 // 3. Запустить тестовый класс через мавен и через IDE.
 // 4. Закоммитить изменения, залить их на репозиторий GitHub и прислать ссылку.
--- src/test/java/lesson08/homeWork/MainPage.java
+++ src/test/java/lesson08/homeWork/MainPage.java
@@ -1,8 +1,15 @@
 package lesson08.homeWork;
 
+import com.oracle.tools.packager.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import utils.CredsManager;
 
 class MainPage extends BasePage implements MainPageLocators {
+
+    private static final Logger LOG = LogManager.getLogger(FirstTest.class);
 
     private WebDriver driver;
 
@@ -10,14 +17,24 @@ class MainPage extends BasePage implements MainPageLocators {
         super(driver);
     }
 
-    void enterQuery(String query){
+    void enterQuery(String query) {
+
+        LOG.info("Method enterQuery has been called with parameter query {}.", query);
+
         $(FIELD_QUERY_LOCATOR).click();
         $(FIELD_QUERY_LOCATOR).clear();
         $(FIELD_QUERY_LOCATOR).sendKeys(query);
     }
 
-    void visit(){
+    void visit() {
+        LOG.info("Page automationpractice.com was visited.");
         open("http://automationpractice.com/index.php");
     }
 
+    public void login() {
+        $(By.id("email")).sendKeys(CredsManager.getCred("username"));
+        $(By.id("passwd")).sendKeys(CredsManager.getCred("passwd"));
+        $(By.id("SubmitLogin")).click();
+
+    }
 }
